7cc3e430dffe211cd86b002cc18b50109d5c6c79
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-07-06 12:40:39 +0100

    Dessin de la pomme

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function()
     var ctx;
     var delay = 100;
     var snakee;
-
+    var applee;
     init();
     function init()
     {       
@@ -17,6 +17,7 @@ window.onload = function()
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
         snakee = new Snake([[6,4], [5,4], [4,4]], "right");
+        applee = new Apple([10,10]);
         refreshCanvas();
     }
 
@@ -24,7 +25,8 @@ window.onload = function()
     {
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         snakee.draw();
-        snakee.advance()
+        snakee.advance();
+        applee.draw();
         setTimeout(refreshCanvas, delay);
     }
 
@@ -94,6 +96,23 @@ window.onload = function()
             }
         };
     };
+
+    function Apple(position)
+    {
+        this.position = position;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize/2;
+            var x = this.position[0] * blockSize + radius;
+            var y= this.position[1] * blockSize + radius;
+            ctx.arc(x,y, radius, 0, Math.PI*2, true);
+            ctx.fill();
+            ctx.restore();
+        };
+    }
 
     document.onkeydown = function handleKeyDown(e)
     {
